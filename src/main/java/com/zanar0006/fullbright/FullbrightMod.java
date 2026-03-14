5efda47707b80bccaddde924c353a251ece481b1
zanar0006-png <zanar0006@gmail.com>
2026-03-14 16:01:15 +0500

    Create FullbrightMod.java implementing ModInitializer

---
+++ src/main/java/com/zanar0006/fullbright/FullbrightMod.java
@@ -0,0 +1,11 @@
+package com.zanar0006.fullbright;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FullbrightMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This is where the mod initialization code will go.
+        System.out.println("Fullbright mod has been initialized!");
+    }
+}
